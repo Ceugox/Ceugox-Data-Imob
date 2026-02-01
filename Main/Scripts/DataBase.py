f676c56f482c62ba3ccbaf12147864583ba6deec
Marcell Parra <143279940+Ceugox@users.noreply.github.com>
2026-02-01 20:07:28 -0300

    Update database connection string and error handling

--- Main/Scripts/DataBase.py
+++ Main/Scripts/DataBase.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 
 # Sua connection string do Neon
-url_banco = 'postgresql://neondb_owner:npg_ap1GTQBoSF6r@ep-curly-wildflower-ac25jj4a-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require'
+url_banco = 'postgresql://neondb_owner:npg_ap1GTQBo12swA6r@ep-curly-wildflower-ac2578414a-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=123'
 
 engine = create_engine(url_banco)
 
@@ -12,3 +12,4 @@ try:
 except Exception as e:
 
     print(f"Erro: {e}")
+
